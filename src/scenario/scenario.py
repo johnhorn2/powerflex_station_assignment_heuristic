de24b8c0411bfd40683184a4ef0673f74fee7e2b
john.horn <john.horn@powerflex.com>
2022-08-30 10:05:27 -0700

    first config file, wip

--- src/scenario/scenario.py
+++ src/scenario/scenario.py
@@ -1,2 +1,16 @@
+from datetime import datetime
 import random
+from typing import Dict
 
+from pydantic import BaseModel
+
+from src.depot.depot import Depot
+
+class Scenario(BaseModel):
+    current_timestamp: datetime
+    config: Dict
+    depot: Depot = None
+
+
+    def build_depot(self):
+        pass
