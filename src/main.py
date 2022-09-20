3a4d1d2845f3cbf70e8302da31e9f32d3bf6a814
john.horn <john.horn@powerflex.com>
2022-09-20 13:58:58 -0700

    dcfc vs l2 vs kpi

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+from collections import namedtuple
 import pickle
 import json
 import os
@@ -15,16 +16,26 @@ from src.heuristic.depot.algo_depot import AlgoDepot
 from src.mock_queue.mock_queue import MockQueue
 from src.plotter.plotter import Plotter
 
+N_REPEATS = 4
 
 L2_STATION_MIN = 1
-L2_STATION_MAX = 30
-L2_STEPS = 5
-STATIONS = np.linspace(L2_STATION_MIN, L2_STATION_MAX, L2_STEPS, dtype=int)
+L2_STATION_MAX = 15
+L2_STEPS = 1
+# L2_STATIONS = np.linspace(L2_STATION_MIN, L2_STATION_MAX, L2_STEPS, dtype=int)
+L2_STATIONS = np.arange(L2_STATION_MIN, L2_STATION_MAX, L2_STEPS)
 
 VEH_MIN = 5
 VEH_MAX = 30
 VEH_STEPS = 5
-VEHS = np.linspace(VEH_MIN, VEH_MAX, VEH_STEPS, dtype=int)
+
+DCFC_STATION_MIN = 0
+DCFC_STEPS = 1
+DCFC_STATION_MAX = 6
+# DCFC_STATIONS = np.linspace(DCFC_STATION_MIN, DCFC_STATION_MAX, DCFC_STEPS, dtype=int)
+DCFC_STATIONS = np.arange(DCFC_STATION_MIN, DCFC_STATION_MAX, DCFC_STEPS)
+
+
+# VEHS = np.linspace(VEH_MIN, VEH_MAX, VEH_STEPS, dtype=int)
 
 random_sort_list=[True,False]
 
@@ -76,144 +87,121 @@ class RuntimeEnvironment(BaseModel):
             return departure_delta_minutes
 
 output_list = []
-for random_sort in random_sort_list:
-    for veh_count in VEHS:
-        for station_count in STATIONS:
-            print('running with veh_count: ' + str(veh_count) + ' and station_count: ' + str(station_count))
-            # setup mock queue
-            mock_queue = MockQueue(
-                scan_events=[],
-                reservations=[],
-                reservation_assignments=[],
-                move_charge=[],
-                departures=[],
-                walk_in_events=[],
-                vehicles_demand_sim=[],
-                vehicles_heuristic=[],
-                stations=[],
-            )
+for repeat in range(0,N_REPEATS):
+    print('Repeat #: ' + str(repeat))
+    for random_sort in random_sort_list:
+        for dcfc_station_count in DCFC_STATIONS:
+            for station_count in L2_STATIONS:
+                print('running with dcfc_count: ' + str(dcfc_station_count) + ' and l2_station_count: ' + str(station_count))
+                # setup mock queue
+                mock_queue = MockQueue(
+                    scan_events=[],
+                    reservations=[],
+                    reservation_assignments=[],
+                    move_charge=[],
+                    departures=[],
+                    walk_in_events=[],
+                    vehicles_demand_sim=[],
+                    vehicles_heuristic=[],
+                    stations=[],
+                )
 
-            script_dir = os.path.dirname(__file__) #<-- absolute dir the script is in
-            demand_sim_config = 'demand_simulator/demand_simulator_config/configs/5days_15min_40res_per_day.json'
-            demand_sim_path = os.path.join(script_dir, demand_sim_config)
+                script_dir = os.path.dirname(__file__) #<-- absolute dir the script is in
+                demand_sim_config = 'demand_simulator/demand_simulator_config/configs/5days_15min_40res_per_day.json'
+                demand_sim_path = os.path.join(script_dir, demand_sim_config)
 
-            # setup demand_simulator
-            with open(demand_sim_path) as f:
-                config = json.load(f)
-
-                demand_simulator_config = DemandSimulatorConfig(**config)
-                demand_simulator = DemandSimulator(config=demand_simulator_config, queue=mock_queue)
-
-                # setup asset_simulator
-                asset_sim_config = 'asset_simulator/depot_config/configs/150_vehicles_10_L2_2_DCFC.json'
-                asset_sim_path = os.path.join(script_dir, asset_sim_config)
-
-                with open(asset_sim_path) as f:
+                # setup demand_simulator
+                with open(demand_sim_path) as f:
                     config = json.load(f)
 
-                # override params
-                config['vehicles']['sedan']['n'] = veh_count
-                config['n_l2_stations'] = station_count
+                    demand_simulator_config = DemandSimulatorConfig(**config)
+                    demand_simulator = DemandSimulator(config=demand_simulator_config, queue=mock_queue)
 
-                asset_depot_config = AssetDepotConfig(**config)
-                asset_depot = AssetDepot.build_depot(config=asset_depot_config, queue=mock_queue)
-                asset_depot.initialize_plugins()
+                    # setup asset_simulator
+                    asset_sim_config = 'asset_simulator/depot_config/configs/150_vehicles_10_L2_2_DCFC.json'
+                    asset_sim_path = os.path.join(script_dir, asset_sim_config)
 
-                # setup heuristic
-                # add a few more algo specific configs
-                config['minimum_ready_vehicle_pool'] = {
-                    'sedan': 2,
-                    'crossover': 2,
-                    'suv': 2
-                }
-                algo_depot_config = AssetDepotConfig(**config)
+                    with open(asset_sim_path) as f:
+                        config = json.load(f)
 
-                algo_depot = AlgoDepot.build_depot(config=asset_depot_config, queue=mock_queue)
+                    # override params
+                    config['vehicles']['sedan']['n'] = 15
+                    config['n_l2_stations'] = station_count
+                    config['n_dcfc_stations'] = dcfc_station_count
 
+                    asset_depot_config = AssetDepotConfig(**config)
+                    asset_depot = AssetDepot.build_depot(config=asset_depot_config, queue=mock_queue)
+                    asset_depot.initialize_plugins()
 
-                runtime = RuntimeEnvironment(
-                    mock_queue=mock_queue,
-                    demand_simulator=demand_simulator,
-                    asset_simulator=asset_depot,
-                    heuristic=algo_depot,
-                    queue=mock_queue
-                )
-
-                # departure deltas in minutes
-                results = runtime.run(plot_output=False, random_sort=random_sort)
-
-                output_list.append(
-                    {
-                        'business_as_usual': random_sort,
-                        'num_vehicles': veh_count,
-                        'l2_stations': station_count,
-                        'departure_deltas': results
+                    # setup heuristic
+                    # add a few more algo specific configs
+                    config['minimum_ready_vehicle_pool'] = {
+                        'sedan': 2,
+                        'crossover': 2,
+                        'suv': 2
                     }
-                )
+                    algo_depot_config = AssetDepotConfig(**config)
+
+                    algo_depot = AlgoDepot.build_depot(config=asset_depot_config, queue=mock_queue)
 
 
-                # create dictionary for vis dataframe where key: vehicle_id, value: cnt
-                vis_tracker = {}
-                for veh_id, val in runtime.asset_simulator.reservation_assignment_snapshot.items():
-                    vis_tracker[veh_id] = len(val)
+                    runtime = RuntimeEnvironment(
+                        mock_queue=mock_queue,
+                        demand_simulator=demand_simulator,
+                        asset_simulator=asset_depot,
+                        heuristic=algo_depot,
+                        queue=mock_queue
+                    )
 
-                # create dictionary for self.reservation dataframe where key: vehicle_id, value: cnt
-                # res_tracker = {}
-                # for res in runtime.asset_simulator.reservations.values():
-                #     try:
-                #         res_tracker[res.assigned_vehicle_id] += 1
-                #     except KeyError:
-                #         res_tracker[res.assigned_vehicle_id] = 1
-                #
-                # print('resrevations tracker')
-                # for veh_id, cnt in res_tracker.items():
-                #     print('veh:' + str(veh_id) + ', cnt: ' + str(cnt))
+                    # departure deltas in minutes
+                    results = runtime.run(plot_output=False, random_sort=random_sort)
 
-                # departure_tracker = {}
-                # for veh_id in runtime.asset_simulator.departure_snapshot['vehicle_id']:
-                #     try:
-                #         departure_tracker[veh_id] += 1
-                #     except KeyError:
-                #         departure_tracker[veh_id] = 1
-
-                # print('departure tracker')
-                # for veh_id, cnt in departure_tracker.items():
-                #     print('veh:' + str(veh_id) + ', cnt: ' + str(cnt))
-
-                # runtime.asset_simulator.departure_snapshot
-
-                print('simulation complete')
-
-with open('multi_run.pickle', 'wb') as handle:
-    pickle.dump(output_list, handle, protocol=pickle.HIGHEST_PROTOCOL)
-
-with open('multi_run.pickle', 'rb') as handle:
-    multi_run_list = pickle.load(handle)
-
-# process the output for 3d plotting
-heur_ev_cnt = []
-heur_station_cnt = []
-heur_mean_late = []
+                    output_list.append(
+                        {
+                            'business_as_usual': random_sort,
+                            'dcfc_stations': dcfc_station_count,
+                            'l2_stations': station_count,
+                            'departure_deltas': results
+                        }
+                    )
 
 
-bau_ev_cnt = []
-bau_station_cnt = []
-bau_mean_late = []
+                    # create dictionary for vis dataframe where key: vehicle_id, value: cnt
+                    vis_tracker = {}
+                    for veh_id, val in runtime.asset_simulator.reservation_assignment_snapshot.items():
+                        vis_tracker[veh_id] = len(val)
 
-for result_dict in multi_run_list:
+                    print('simulation complete')
+
+
+# we use tuples (ev_cnt, station_cnt) as the key
+Result = namedtuple('Result', ('dcfc_station_cnt', 'station_cnt'))
+bau_result_dict = {}
+heur_result_dict = {}
+
+
+for result_dict in output_list:
     if result_dict['business_as_usual']:
-        bau_ev_cnt.append(result_dict['num_vehicles'])
-        bau_station_cnt.append(result_dict['l2_stations'])
-        bau_mean_late.append(np.mean(result_dict['departure_deltas']))
+        key = Result(result_dict['dcfc_stations'], result_dict['l2_stations'])
+        try:
+            # list instantiated already
+            bau_result_dict[key].append(result_dict['departure_deltas'])
+        except KeyError:
+            # need to initiate list for this new index
+            bau_result_dict[key] = []
+            bau_result_dict[key].append(result_dict['departure_deltas'])
     else:
-        heur_ev_cnt.append(result_dict['num_vehicles'])
-        heur_station_cnt.append(result_dict['l2_stations'])
-        heur_mean_late.append(np.mean(result_dict['departure_deltas']))
+        key = Result(result_dict['dcfc_stations'], result_dict['l2_stations'])
+        try:
+            # list instantiated already
+            heur_result_dict[key].append(result_dict['departure_deltas'])
+        except KeyError:
+            # need to initiate list for this new index
+            heur_result_dict[key] = []
+            heur_result_dict[key].append(result_dict['departure_deltas'])
 
+with open('plotter/data_from_prior_runs/heuristic_result.pickle', 'wb') as handle:
+    pickle.dump(heur_result_dict, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
-# x, y = np.linspace(0, 1, sh_0), np.linspace(0, 1, sh_1)
-fig = go.Figure(data=[go.Surface(z=heur_mean_late, x=heur_station_cnt, y=heur_station_cnt)])
-fig.update_layout(title='Mean Departure Tardiness (minutes)', autosize=False,
-                  width=500, height=500,
-                  margin=dict(l=65, r=50, b=65, t=90))
-fig.show()
+with open('plotter/data_from_prior_runs/bau_result.pickle', 'wb') as handle:
+    pickle.dump(bau_result_dict, handle, protocol=pickle.HIGHEST_PROTOCOL)
--- src/plotter/plot_multi_run.py
+++ src/plotter/plot_multi_run.py
@@ -1,41 +1,68 @@
+from collections import namedtuple
 import pickle
 
 import numpy as np
 import plotly.graph_objects as go
 
-with open('../multi_run.pickle', 'rb') as handle:
-    multi_run_list = pickle.load(handle)
+Result = namedtuple('Result', ('dcfc_station_cnt', 'station_cnt'))
+
+with open('data_from_prior_runs/heuristic_result.pickle', 'rb') as handle:
+    heuristic_raw_results = pickle.load(handle)
+
+with open('data_from_prior_runs/bau_result.pickle', 'rb') as handle:
+    bau_raw_results = pickle.load(handle)
 
 # process the output for 3d plotting
-heur_ev_cnt = []
-heur_station_cnt = []
-heur_mean_late = []
+heuristic = {}
+heuristic['dcfc_station_cnt'] = []
+heuristic['station_cnt'] = []
+heuristic['mean_tardiness'] = []
+
+bau = {}
+bau['dcfc_station_cnt'] = []
+bau['station_cnt'] = []
+bau['mean_tardiness'] = []
 
 
-bau_ev_cnt = []
-bau_station_cnt = []
-bau_mean_late = []
+# need to find the average of all the entries in the values and assign to a list
+for keys, values in heuristic_raw_results.items():
+    heuristic['dcfc_station_cnt'].append(keys.dcfc_station_cnt)
+    heuristic['station_cnt'].append(keys.station_cnt)
+    flat_list = [item for sublist in values for item in sublist]
+    heuristic['mean_tardiness'].append(np.mean(flat_list))
 
-print(multi_run_list)
+for keys, values in heuristic_raw_results.items():
+    bau['dcfc_station_cnt'].append(keys.dcfc_station_cnt)
+    bau['station_cnt'].append(keys.station_cnt)
+    flat_list = [item for sublist in values for item in sublist]
+    bau['mean_tardiness'].append(np.mean(flat_list))
 
-for result_dict in multi_run_list:
-    if result_dict['business_as_usual']:
-        bau_ev_cnt.append(result_dict['num_vehicles'])
-        bau_station_cnt.append(result_dict['l2_stations'])
-        bau_mean_late.append(np.mean(result_dict['departure_deltas']))
-    else:
-        heur_ev_cnt.append(result_dict['num_vehicles'])
-        heur_station_cnt.append(result_dict['l2_stations'])
-        heur_mean_late.append(np.mean(result_dict['departure_deltas']))
-
-print(heur_ev_cnt)
-print(heur_station_cnt)
-print(heur_mean_late)
+L2_STATION_MIN = 1
+L2_STATION_MAX = 15
+L2_STEPS = 1
+L2_STATIONS = np.arange(L2_STATION_MIN, L2_STATION_MAX, L2_STEPS)
 
 
-# x, y = np.linspace(0, 1, sh_0), np.linspace(0, 1, sh_1)
-fig = go.Figure(data=[go.Surface(z=heur_mean_late, x=heur_station_cnt, y=heur_station_cnt)])
-fig.update_layout(title='Mean Departure Tardiness (minutes)', autosize=False,
+DCFC_STATION_MIN = 0
+DCFC_STEPS = 1
+DCFC_STATION_MAX = 6
+DCFC_STATIONS = np.arange(DCFC_STATION_MIN, DCFC_STATION_MAX, DCFC_STEPS)
+
+new_shape = (len(L2_STATIONS), len(DCFC_STATIONS))
+heuristic_result = np.reshape(heuristic['mean_tardiness'], newshape=(new_shape))
+bau_result = np.reshape(bau['mean_tardiness'], newshape=new_shape)
+
+
+fig = go.Figure(data=[go.Surface(z=heuristic_result, x=L2_STATIONS, y=DCFC_STATIONS)])
+fig.update_layout(title='Mean Departure Tardiness (minutes)',autosize=True,
                   width=500, height=500,
-                  margin=dict(l=65, r=50, b=65, t=90))
+                  margin=dict(l=65, r=50, b=65, t=90),
+                  )
+fig.update_layout(scene = dict(
+                    xaxis_title='# L2 Stations',
+                    yaxis_title='# DCFC Stations',
+                    zaxis_title='Avg Minutes Late for Deptarture'),
+                    width=700,
+                    margin=dict(r=20, b=10, l=10, t=10))
+
 fig.show()
